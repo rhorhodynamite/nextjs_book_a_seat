6a72775596ac301a55d49cdc656203e64ee3d729
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-08 14:19:03 +0200

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,7 +2,7 @@
 import Cors from 'cors'
 const cors = Cors({
   methods: ['POST', 'GET', 'HEAD'],
-  origin: ['https://book-a-seat-hdkl7ux7m-rhorhodynamites-projects.vercel.app', 'http://localhost:3000'],
+  origin: ['https://book-a-seat-eta.vercel.app/', 'http://localhost:3000'],
   credentials: true
 })
 
