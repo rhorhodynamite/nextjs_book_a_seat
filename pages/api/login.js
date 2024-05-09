cae5bb38704b2c87e39d904c780452a1592e73e9
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-09 11:16:47 +0200

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -15,6 +15,7 @@ export default function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', 'https://book-a-seat-eta.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Cache-Control, Expires, Pragma');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
 
     // Handle OPTIONS method for CORS preflight
     if (req.method === 'OPTIONS') {
