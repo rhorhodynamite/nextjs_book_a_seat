9818544f0b544a77125dbc65e1c20822248074f7
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-09 18:17:11 +0200

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,37 +1,42 @@
 import crypto from 'crypto';
-import { cors, runMiddleware } from '../../lib/db';
+import {conn, cors, runMiddleware } from '../../lib/db';
+import { sign } from 'jsonwebtoken';
 
-const fakeLogin = [
-  { user: "admin0", pwd: "admin0", role: "admin" },
-  { user: "user1", pwd: "user1", role: "user" },
-  { user: "user2", pwd: "user2", role: "user" },
-  { user: "user3", pwd: "user3", role: "user" },
-];
+
+const saltRounds = 10; // Define the number of salt rounds for bcrypt
 
 export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
-  // console.log('login', req.method);
-  // const foundItem = fakeLogin.find(item => {
-  //   if (item.user === req.body.user && item.pwd === req.body.pwd) {
-  //     return item;
-  //   }
-  //   return null;
-  // });
 
-  // temporary solution
-  const foundItem = {user: req.body.user, pwd:"user1", role:"user"}
-  if (req.body.user == "admin0"){
-    foundItem.role = 'admin';
+  const { username, password } = req.body;
+
+  try {
+    // Query the users table for the provided username
+    const queryResult = await conn.query('SELECT * FROM book_a_seat.users WHERE username = $1', [username]);
+    const user = queryResult.rows[0];
+
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    // Compare the provided password with the hashed password stored in the database
+    const passwordMatch = await compare(password, user.hashed_password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    // If the credentials are valid, generate a JWT token
+    const token = sign({ userId: user.id, username: user.username }, 'your-secret-key');
+
+    res.status(200).json({ token, role: user.role, username: user.username });
+  } catch (error) {
+    console.error('Error authenticating user:', error);
+    res.status(500).json({ error: 'Internal server error' });
   }
-  // console.log('foundItem', foundItem);
-  if (foundItem){
-      const buffer = crypto.randomBytes(48);
-      res.status(200).json({ 
-        token: buffer.toString('hex'),
-        role: foundItem.role,
-        user: foundItem.user 
-      });
-  } else {
-    res.status(200).json({ token: null, role: 'user', user: null});
-  }
+}
+
+// Hash the provided password before storing it in the database
+export async function hashPassword(password) {
+  return bcrypt.hash(password, saltRounds);
 }
