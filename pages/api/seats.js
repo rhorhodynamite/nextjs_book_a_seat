4af42bd2e807fcff28079f08b0d2afaeaeec37ff
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-14 10:52:52 +0200

    Update seats.js

--- pages/api/seats.js
+++ pages/api/seats.js
@@ -1,62 +1,50 @@
-import {conn, cors, runMiddleware} from '../../lib/db';
+import { conn, cors, runMiddleware } from '../../lib/db';
 
-/**
- *  GET to retrieve a resource;
- *  PUT to change the state of or update a resource, which can be an object, file or block;
- *  POST to create that resource; and
- *  DELETE to remove it.
- * @param {*} req 
- * @param {*} res 
- */
 export default async function handler(req, res) {
-  // console.log(req.method);
   await runMiddleware(req, res, cors);
+
   if (req.method === 'OPTIONS') {
-    res.status(200)
+    res.status(200);
   }
+
   if (req.method === 'GET') {
-    const seatRslt = await conn.query('SELECT * FROM  book_a_seat.seat_objs'/*, values*/);
-    const tableRslt = await conn.query('SELECT * FROM  book_a_seat.table_objs'/*, values*/);;
+    const { svgType } = req.query;
+    const seatRslt = await conn.query('SELECT * FROM book_a_seat.seat_objs WHERE svgType = $1', [svgType]);
+    const tableRslt = await conn.query('SELECT * FROM book_a_seat.table_objs WHERE svgType = $1', [svgType]);
 
     res.status(200).json({
       seats: seatRslt.rows,
       tables: tableRslt.rows
     });
-  } else {
-    // console.log('not get........');
-    let successfull = false;
-    try{
+  } else if (req.method === 'POST') {
+    let successful = false;
+    try {
       const reqObjs = req.body;
-      // console.log('1');
-      if(reqObjs){
-        await conn.query(`DELETE FROM book_a_seat.seat_objs`);
-        // console.log('2');
-        reqObjs.seats.forEach(async (seat)=>{
-          // console.log(`INSERT INTO book_a_seat.seat_objs (id, name, x, y)  
-          // VALUES (${seat.id}, '${seat.name}', ${seat.x}, ${seat.y})`);
-          // console.log('3');
-          await conn.query(
-            `INSERT INTO book_a_seat.seat_objs (id, name, x, y)  
-            VALUES (${seat.id}, '${seat.name}', ${seat.x}, ${seat.y})`);
-        });
+      const { svgType } = req.query;
 
-        // console.log('4');
-        await conn.query(`DELETE FROM book_a_seat.table_objs`);
-        reqObjs.tables.forEach(async (table)=>{
-          // console.log(`INSERT INTO book_a_seat.seat_objs (id, name, x, y)  
-          // VALUES (${seat.id}, '${seat.name}', ${seat.x}, ${seat.y})`);
-          // console.log('5');
+      if (reqObjs) {
+        await conn.query('DELETE FROM book_a_seat.seat_objs WHERE svgType = $1', [svgType]);
+        for (const seat of reqObjs.seats) {
           await conn.query(
-            `INSERT INTO book_a_seat.table_objs (id, name, x, y, width, height)  
-            VALUES (${table.id}, '${table.name}', ${table.x}, ${table.y}, ${table.width}, ${table.height})`);
-        });
-        successfull = true;
+            'INSERT INTO book_a_seat.seat_objs (id, name, x, y, svgType) VALUES ($1, $2, $3, $4, $5)',
+            [seat.id, seat.name, seat.x, seat.y, svgType]
+          );
+        }
+
+        await conn.query('DELETE FROM book_a_seat.table_objs WHERE svgType = $1', [svgType]);
+        for (const table of reqObjs.tables) {
+          await conn.query(
+            'INSERT INTO book_a_seat.table_objs (id, name, x, y, width, height, svgType) VALUES ($1, $2, $3, $4, $5, $6, $7)',
+            [table.id, table.name, table.x, table.y, table.width, table.height, svgType]
+          );
+        }
+        successful = true;
       }
     } catch (error) {
       console.error(error);
     }
     res.status(200).json({
-      successfull: successfull
+      successful: successful
     });
   }
 }
