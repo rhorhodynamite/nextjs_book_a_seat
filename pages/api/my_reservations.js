d26d00332b5f4c168acf299b85039aaf187a3384
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-15 17:33:44 +0200

    Update my_reservations.js

--- pages/api/my_reservations.js
+++ pages/api/my_reservations.js
@@ -9,19 +9,23 @@ export default async function handler(req, res) {
     let rslt = null;
     console.log('method get', id);
     if (id) {
-      // Fetch bookings for a specific user
+      // Fetch bookings for a specific user with seat name
       rslt = await conn.query(`SELECT r.id, r.seat_id as seatId, r.username, 
                                       to_char(r.start_date, 'YYYY-MM-DD HH24:MI:SS') as startDate, 
-                                      to_char(r.end_date, 'YYYY-MM-DD HH24:MI:SS') as endDate
+                                      to_char(r.end_date, 'YYYY-MM-DD HH24:MI:SS') as endDate,
+                                      s.name as seatName
                                FROM book_a_seat.reservation r
+                               JOIN book_a_seat.seat_objs s ON r.seat_id = s.id
                                WHERE r.username = $1
                                ORDER BY r.start_date`, [id]);
     } else {
-      // Fetch all bookings
+      // Fetch all bookings with seat name
       rslt = await conn.query(`SELECT r.id, r.seat_id as seatId, r.username, 
                                       to_char(r.start_date, 'YYYY-MM-DD HH24:MI:SS') as startDate, 
-                                      to_char(r.end_date, 'YYYY-MM-DD HH24:MI:SS') as endDate
+                                      to_char(r.end_date, 'YYYY-MM-DD HH24:MI:SS') as endDate,
+                                      s.name as seatName
                                FROM book_a_seat.reservation r
+                               JOIN book_a_seat.seat_objs s ON r.seat_id = s.id
                                ORDER BY r.start_date`);
     }
     res.status(200).json({
@@ -29,5 +33,4 @@ export default async function handler(req, res) {
     });
   }
 }
-
 
