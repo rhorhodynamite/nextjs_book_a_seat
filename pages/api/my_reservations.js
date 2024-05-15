01fc2072eaa4ed8acead92972b8597046c7a81db
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-15 17:28:15 +0200

    Update my_reservations.js

--- pages/api/my_reservations.js
+++ pages/api/my_reservations.js
@@ -17,13 +17,11 @@ export default async function handler(req, res) {
                                WHERE r.username = $1
                                ORDER BY r.start_date`, [id]);
     } else {
-      // Fetch bookings for all users overlapping today
+      // Fetch all bookings
       rslt = await conn.query(`SELECT r.id, r.seat_id as seatId, r.username, 
                                       to_char(r.start_date, 'YYYY-MM-DD HH24:MI:SS') as startDate, 
                                       to_char(r.end_date, 'YYYY-MM-DD HH24:MI:SS') as endDate
                                FROM book_a_seat.reservation r
-                               WHERE r.start_date <= CURRENT_DATE + interval '1 day' 
-                               AND r.end_date >= CURRENT_DATE - interval '1 day'
                                ORDER BY r.start_date`);
     }
     res.status(200).json({
