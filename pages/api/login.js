f63d5107d00f29bd537d805a169a2f7abee91d19
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-23 15:38:37 +0200

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,15 +1,11 @@
-import crypto from 'crypto';
 import {conn, cors, runMiddleware } from '../../lib/db';
 import { sign } from 'jsonwebtoken';
-
-
-const saltRounds = 10; // Define the number of salt rounds for bcrypt
+import { scryptSync } from 'crypto';
 
 export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
 
   const { user: username, pwd: password } = req.body;
-
 
   try {
     // Query the users table for the provided username
@@ -20,12 +16,13 @@ export default async function handler(req, res) {
       return res.status(401).json({ error: 'Invalid username or password' });
     }
 
-    // Compare the provided password with the hashed password stored in the database
-    //const passwordMatch = await compare(password, user.hashed_password);
+    // Hash the provided password with the stored salt
+    const hashedPassword = scryptSync(password, user.salt, 64).toString('hex');
 
-    //if (!passwordMatch) {
-    //  return res.status(401).json({ error: 'Invalid username or password' });
-    //}
+    // Compare the provided password's hash with the stored hashed password
+    if (hashedPassword !== user.hashed_password) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
 
     // If the credentials are valid, generate a JWT token
     const token = sign({ userId: user.id, username: user.username }, 'your-secret-key');
@@ -37,7 +34,3 @@ export default async function handler(req, res) {
   }
 }
 
-// Hash the provided password before storing it in the database
-//export async function hashPassword(password) {
-//  return bcrypt.hash(password, saltRounds);
-//}
